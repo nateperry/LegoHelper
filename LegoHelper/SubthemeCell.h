bbfd4de58e62ddb6b7e99ad65277fd6a29e57132
nateperry <ndp4570@rit.edu>
2014-11-23 17:39:30 -0500

    added subtheme cell view

---
+++ LegoHelper/SubthemeCell.h
@@ -0,0 +1,13 @@
+//
+//  SubthemeCell.h
+//  LegoHelper
+//
+//  Created by Student on 11/23/14.
+//  Copyright (c) 2014 Nate Perry and Nate Kierpiec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubthemeCell : UITableViewCell
+
+@end
